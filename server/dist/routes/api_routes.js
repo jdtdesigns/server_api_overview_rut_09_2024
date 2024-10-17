2ca384055343cddcd15d6fd1ade6d82f80ff8fba
JD Tadlock <jt7903@gmail.com>
2024-10-17 14:53:35 -0400

    ready to deploy quote api

---
+++ server/dist/routes/api_routes.js
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import axios from 'axios';
+const router = Router();
+const people = [
+    {
+        id: 1,
+        name: 'JD'
+    },
+    {
+        id: 2,
+        name: 'Sarah'
+    },
+    {
+        id: 3,
+        name: 'Bob'
+    }
+];
+// Get Person By ID
+router.get('/api/person/:personId', (requestObj, responseObj) => {
+    console.log(requestObj.params.personId);
+    const person = people.find((personObj) => {
+        if (personObj.id == Number(requestObj.params.personId)) {
+            return personObj;
+        }
+        return false;
+    });
+    responseObj.json(person || {
+        message: 'A person with that ID could not be found.'
+    });
+});
+// Random quote GET route
+router.get('/api/quote', async (requestObj, responseObj) => {
+    // console.log(requestObj.query);
+    if (!requestObj.query.cat) {
+        return responseObj.json({
+            message: 'You must provide a cat parameter with the category that you would like.'
+        });
+    }
+    const url = `https://api.api-ninjas.com/v1/quotes?category=${requestObj.query.cat}`;
+    try {
+        const res = await axios.get(url, {
+            headers: {
+                'X-Api-Key': process.env.API_KEY
+            }
+        });
+        responseObj.json({
+            quote: res.data[0].quote
+        });
+    }
+    catch (error) {
+        responseObj.json({
+            message: 'You must type one of the known categories.'
+        });
+    }
+});
+export default router;
